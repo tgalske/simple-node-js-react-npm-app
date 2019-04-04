0aec9d7aea080bf2f170fa4a279bd2e8ddc0de05
Tyler <tgalske@gmail.com>
2019-04-04 17:57:13 -0400

    Changes for testing env var

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    console.log(process.env);
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Message: {process.env.react_app_message}</p>
       </div>
     );
   }
